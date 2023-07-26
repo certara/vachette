84748e3d72c4dae7df0f7f0747f6750b5f4d7b66
certara-jcraig <61294078+certara-jcraig@users.noreply.github.com>
2023-07-26 07:58:17 -0700

    update tests

--- tests/testthat/test_vachette_data.R
+++ tests/testthat/test_vachette_data.R
@@ -5,18 +5,29 @@ test_that("vachette curves are accurate for iv", {
   vd <- vachette_data(
     obs.data = read.csv(system.file(package = "vachette", "examples", "iv-obs.csv")),
     typ.data = read.csv(system.file(package = "vachette", "examples", "iv-typ.csv")),
-    covariates = c("vachette.cov1" = 70),
+    covariates = c(WT = 70),
+    mappings = c(OBS = "DV",
+                 x = "time"),
     ref.dosenr = 1
   )
 
   reference <-
     read.csv(
-      system.file(package = "vachette", "test_files", "vachette-curves-iv.csv"),
+      system.file(package = "vachette", "test_files", "obs-curves-iv.csv"),
       colClasses = c(COV = "character", ucov = "double"),
       numerals = "no.loss"
     )
 
-  testthat::expect_identical(vd$typ.data, reference)
+  testthat::expect_equal(vd$obs.orig, tidyr::as_tibble(reference))
+
+  reference <-
+    read.csv(
+      system.file(package = "vachette", "test_files", "typ-curves-iv.csv"),
+      colClasses = c(COV = "character", ucov = "double"),
+      numerals = "no.loss"
+    )
+
+  testthat::expect_equal(vd$typ.orig, tidyr::as_tibble(reference))
 
 })
 
@@ -28,17 +39,28 @@ test_that("vachette curves are accurate for oral-two-cov", {
   vd <- vachette_data(
     obs.data = read.csv(system.file(package = "vachette", "examples", "oral-two-cov-obs.csv")),
     typ.data = read.csv(system.file(package = "vachette", "examples", "oral-two-cov-typ.csv")),
-    covariates =  c("vachette.cov1" = 70, "vachette.cov2" = 30),
+    covariates =  c(WT = 70, AGE = 30),
+    mappings = c(OBS = "DV",
+                 x = "time"),
     ref.dosenr = 1
   )
 
   reference <-
     read.csv(
-      system.file(package = "vachette", "test_files", "vachette-curves-oral-two-cov-v1.csv"),
+      system.file(package = "vachette", "test_files", "obs-curves-oral-two-cov.csv"),
       colClasses = c(COV = "character", ucov = "double"),
       numerals = "no.loss"
     )
 
-  testthat::expect_identical(vd$typ.data, reference)
+  testthat::expect_equal(vd$obs.orig, tidyr::as_tibble(reference))
+
+  reference <-
+    read.csv(
+      system.file(package = "vachette", "test_files", "typ-curves-oral-two-cov.csv"),
+      colClasses = c(COV = "character", ucov = "double"),
+      numerals = "no.loss"
+    )
+
+  testthat::expect_equal(vd$typ.orig, tidyr::as_tibble(reference))
 
 })
