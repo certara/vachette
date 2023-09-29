bc2b5e092a6a8e4fa8a6f54598f60560043c4b49
certara-jcraig <61294078+certara-jcraig@users.noreply.github.com>
2023-09-28 17:36:05 -0700

    add pembro data

--- R/data.R
+++ R/data.R
@@ -61,5 +61,67 @@
 #' }
 "oral_absorption_sim"
 
+#' Pembro Observed Data
+#'
+#' This dataset contains a single covariate \code{WT} and a single dose.
+#' The dose event row is recorded by the \code{EVID} column.
+#'
+#' @format A data frame with the following columns:
+#' \describe{
+#'   \item{\code{ID}}{Subject ID}
+#'   \item{\code{time}}{Time used as x-variable}
+#'   \item{\code{EVID}}{Dose event column}
+#'   \item{\code{DV}}{Dependent variable}
+#'   \item{\code{PRED}}{Population predicted value}
+#'   \item{\code{WT}}{Weight covariate}
+#' }
+#' @examples
+#' \dontrun{
+#' data(oral_absorption_obs)
+#' head(oral_absorption_obs)
+#' }
+"pembro_obs"
+
+#' Pembro Typical Curves Data
+#'
+#' This dataset contains a single covariate \code{WT} and a single dose.
+#' The dose event row is recorded by the \code{EVID} column.
+#'
+#' @format A data frame with the following columns:
+#' \describe{
+#'   \item{\code{ID}}{Subject ID}
+#'   \item{\code{time}}{Time used as x-variable}
+#'   \item{\code{EVID}}{Dose event column}
+#'   \item{\code{PRED}}{Population predicted value}
+#'   \item{\code{WT}}{Weight covariate}
+#' }
+#' @examples
+#' \dontrun{
+#' data(oral_absorption_typ)
+#' head(oral_absorption_typ)
+#' }
+"pembro_typ"
+
+#' Pembro Simulated Data
+#'
+#' Simulated dataset containing 100 replicates. Time points are
+#' simulated given observation time points in  \code{\link{oral_absorption_obs}}.
+#'
+#' @format A data frame with the following columns:
+#' \describe{
+#'   \item{\code{REP}}{Replicate number}
+#'   \item{\code{ID}}{Subject ID}
+#'   \item{\code{time}}{Time used as x-variable}
+#'   \item{\code{EVID}}{Dose event column}
+#'   \item{\code{DV}}{Dependent variable}
+#'   \item{\code{PRED}}{Population predicted value}
+#'   \item{\code{WT}}{Weight covariate}
+#' }
+#' @examples
+#' \dontrun{
+#' data(oral_absorption_sim)
+#' head(oral_absorption_sim)
+#' }
+"pembro_sim"
 
 
